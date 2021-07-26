8476bcb39f0233a57b7210885e4deae956a74403
joesephx2 <58317642+joesephx2@users.noreply.github.com>
2021-07-25 23:35:51 -0600

    knex fix

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.BACKEND_PORT || 3001;
+const port = process.env.PORT || 3001;
 const knex = require('knex')(require('../knexfile.js')['development']);
 const cors = require('cors');
 
